668ed5933fd7cef0b6677eb71e8ac920da262b25
hedleyroos <hedleyroos@gmail.com>
2016-02-03 09:59:53 +0200

    Rewrite inefficient use of get into get_many

--- ultracache/utils.py
+++ ultracache/utils.py
@@ -6,35 +6,68 @@ def cache_meta(request, cache_key, start_index=0):
     in Django's cache.  """
 
     path = request.get_full_path()
+
+    # Lists needed for cache.get_many
+    to_set_get_keys = []
+    to_set_paths_get_keys = []
+    to_set_content_types_get_keys = []
+    to_set_content_types_paths_get_keys = []
+
+
+    # Dictionaries needed for cache.set_many
     to_set = {}
     to_set_paths = {}
     to_set_content_types = {}
     to_set_content_types_paths = {}
+
     to_set_objects = []
+
     for ctid, obj_pk in request._ultracache[start_index:]:
         key = 'ucache-%s-%s' % (ctid, obj_pk)
-        to_set.setdefault(key, cache.get(key, []))
-        if cache_key not in to_set[key]:
-            to_set[key].append(cache_key)
+        if key not in to_set_get_keys:
+            to_set_get_keys.append(key)
 
         key = 'ucache-pth-%s-%s' % (ctid, obj_pk)
-        to_set_paths.setdefault(key, cache.get(key, []))
-        if path not in to_set_paths[key]:
-            to_set_paths[key].append(path)
+        if key not in to_set_paths_get_keys:
+            to_set_paths_get_keys.append(key)
 
         key = 'ucache-ct-%s' % ctid
-        to_set_content_types.setdefault(key, cache.get(key, []))
-        if ctid not in to_set_content_types[key]:
-            to_set_content_types[key].append(cache_key)
+        if key not in to_set_content_types_get_keys:
+            to_set_content_types_get_keys.append(key)
 
         key = 'ucache-ct-pth-%s' % ctid
-        to_set_content_types_paths.setdefault(key, cache.get(key, []))
-        if path not in to_set_content_types_paths[key]:
-            to_set_content_types_paths[key].append(path)
+        if key not in to_set_content_types_paths_get_keys:
+            to_set_content_types_paths_get_keys.append(key)
 
         tu = (ctid, obj_pk)
         if tu not in to_set_objects:
             to_set_objects.append(tu)
+
+    # todo: rewrite to handle absence of get_many
+    di = cache.get_many(to_set_get_keys)
+    for key in to_set_get_keys:
+        to_set.setdefault(key, di.get(key, []))
+        if cache_key not in to_set[key]:
+            to_set[key].append(cache_key)
+
+    di = cache.get_many(to_set_paths_get_keys)
+    for key in to_set_paths_get_keys:
+        to_set_paths.setdefault(key, di.get(key, []))
+        if path not in to_set_paths[key]:
+            to_set_paths[key].append(path)
+
+    di = cache.get_many(to_set_content_types_get_keys)
+    for key in to_set_content_types_get_keys:
+        to_set_content_types.setdefault(key, di.get(key, []))
+        ctid = key.split('-')[-1]
+        if ctid not in to_set_content_types[key]:
+            to_set_content_types[key].append(cache_key)
+
+    di = cache.get_many(to_set_content_types_paths_get_keys)
+    for key in to_set_content_types_paths_get_keys:
+        to_set_content_types_paths.setdefault(key, di.get(key, []))
+        if path not in to_set_content_types_paths[key]:
+            to_set_content_types_paths[key].append(path)
 
     if to_set:
         try:
