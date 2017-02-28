43d5807c43c956f53eb797d1a93d246290af3a89
hedleyroos <hedleyroos@gmail.com>
2017-02-28 17:32:20 +0200

    Commit memcache setting

--- ultracache/tests/settings/19.py
+++ ultracache/tests/settings/19.py
@@ -26,11 +26,6 @@ INSTALLED_APPS = (
 )
 
 CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-    }
-}
-CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
