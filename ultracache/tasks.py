c4aee823fda69286f4a08fa23318585451f80282
hedleyroos <hedleyroos@gmail.com>
2018-05-24 11:24:33 +0200

    The task now works again

--- ultracache/tasks.py
+++ ultracache/tasks.py
@@ -1,9 +1,10 @@
 import json
-import urllib
 try:
     from urllib.parse import urlparse
+    from urllib.parse import quote
 except ImportError:
-    import urlparse
+    from urlparse import urlparse
+    from urllib import quote
 
 from celery import shared_task
 try:
@@ -18,7 +19,7 @@ from django.conf import settings
 @shared_task(max_retries=3, ignore_result=True)
 def broadcast_purge(path, headers=None):
     if not DO_TASK:
-        raise RuntimeError("Library pika==0.10.0 not found")
+        raise RuntimeError("Library pika>=0.11,<1.0 not found")
 
     try:
         url = settings.ULTRACACHE["rabbitmq-url"]
@@ -27,11 +28,11 @@ def broadcast_purge(path, headers=None):
         # encoded. A typical broker URL setting remains effectively
         # unchanged but as soon as sub-paths are encountered this encoding
         # becomes necessary.
-        parsed = urlparse.urlparse(settings.CELERY_BROKER_URL)
+        parsed = urlparse(settings.CELERY_BROKER_URL)
         url = "%s://%s/%s" % (
             parsed.scheme,
             parsed.netloc,
-            urllib.quote(parsed.path[1:], safe="")
+            quote(parsed.path[1:], safe="")
         )
     connection = pika.BlockingConnection(pika.URLParameters(url))
     channel = connection.channel()
