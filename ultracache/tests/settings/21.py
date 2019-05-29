e87510347e7a8141068b98b463782d07eeb2c6b7
hedleyroos <hedleyroos@gmail.com>
2019-05-29 10:18:45 +0200

    Add Django 2.1 tests

---
+++ ultracache/tests/settings/21.py
@@ -0,0 +1,55 @@
+import os
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+    }
+}
+
+ROOT_URLCONF = "ultracache.tests.urls"
+
+INSTALLED_APPS = (
+    "test_without_migrations",
+    "ultracache",
+    # Include tests because it declares models
+    "ultracache.tests",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "crum",
+    "rest_framework"
+)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
+SECRET_KEY = "SECRET_KEY"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": False,
+        "OPTIONS": {
+            "context_processors": ["django.template.context_processors.request"],
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ]
+        },
+    },
+]
+
+ULTRACACHE = {
+    "purge": {"method": "ultracache.tests.utils.dummy_purger"},
+    "drf": {"viewsets": {"*": {}}},
+    "consider-headers": ["cookie"],
+}
